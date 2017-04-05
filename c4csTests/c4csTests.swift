57ae462bfaeaed415785a33f94f89623135e3bbb
Anna <schmian@umich.edu>
2017-04-04 23:09:30 -0400

    Initial Commit

---
+++ c4csTests/c4csTests.swift
@@ -0,0 +1,36 @@
+//
+//  c4csTests.swift
+//  c4csTests
+//
+//  Created by Anna Schmidt on 4/4/17.
+//  Copyright © 2017 Anna Schmidt. All rights reserved.
+//
+
+import XCTest
+@testable import c4cs
+
+class c4csTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
